73ec0677317c40563d86120d6614b8976e1f67ba
Ivan Mihaylov <77852160+IvanNMihaylov@users.noreply.github.com>
2021-01-22 19:39:42 +0200

    Add files via upload

---
+++ DuplicateCharacters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BFlow_Tasks
+{
+    class DuplicateCharacters
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int count;
+
+            char[] stringInput = input.ToCharArray();
+
+            Console.WriteLine("Duplicate characters in a given string:");
+
+            for (int i = 0; i < stringInput.Length; i++)
+            {
+                count = 1;
+                for (int j = i+1; j < stringInput.Length; j++)
+                {
+                    if (stringInput[i] == stringInput[j] && stringInput[i]!=' ')
+                    {
+                        count++;
+                        stringInput[j] = '0';
+                    }
+                }
+
+                if (count > 1 && stringInput[i] != '0')
+                {
+                   Console.WriteLine("\"{0}\" occurs {1} times", stringInput[i], count);
+                }
+
+            }
+        }
+    }
+}
