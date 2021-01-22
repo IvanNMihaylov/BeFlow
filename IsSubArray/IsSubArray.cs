0708a425f1a42de461de92859d5f1493e8f22fc6
Ivan Mihaylov <77852160+IvanNMihaylov@users.noreply.github.com>
2021-01-22 19:47:16 +0200

    Add files via upload

---
+++ IsSubArray/IsSubArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubArray
+{
+    class IsSubArray
+    {
+        public static bool IssubArray(int[] arrayA, int[] arrayB)
+        {
+            int startIndexB = 0;
+            foreach (int n in arrayA)
+            {
+                int next = Array.IndexOf(arrayB, n);
+                if (next == -1)
+                {
+                    startIndexB = next + 1;
+
+                }
+                else
+                {
+                    Console.WriteLine("Number : {0}", n);
+                }
+
+            }
+            return true;
+        }
+    }
+}
